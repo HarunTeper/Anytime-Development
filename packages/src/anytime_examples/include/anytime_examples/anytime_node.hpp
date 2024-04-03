6dc6b0be1a01c63e71a755650a578cab36ed98eb
HarunTeper <hteper@web.de>
2024-04-03 10:13:08 +0000

    removed all yolo references, only anytime node left

---
+++ packages/src/anytime_examples/include/anytime_examples/anytime_node.hpp
@@ -0,0 +1,17 @@
+#include "rclcpp/rclcpp.hpp"
+
+class AnytimeST : public rclcpp::Node
+{
+    public:
+        AnytimeST();
+
+    private:
+};
+
+class AnytimeMT : public rclcpp::Node
+{
+    public:
+        AnytimeMT();
+
+    private:
+};
